783e6f15eddc34f14cf7b3c7402b4fc2f4440d1a
dbernardos <121075844+dbernardos@users.noreply.github.com>
2026-03-18 18:27:24 -0300

    models sistema de vendas

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Perfil, Venda, ItemVenda
 
-admin.site.register(Produto)
+@admin.register(Produto)
+class ProdutoAdm(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'preco', 'qtde', 'data')
+    list_display_links = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('preco', 'qtde')
+    list_editable = ('preco', 'qtde')
+
+@admin.register(Perfil)
+class PerfilAdm(admin.ModelAdmin):
+    list_display = ('id', 'telefone', 'rua', 'numero', 'cidade')
+
+@admin.register(Venda)
+class VendaAdm(admin.ModelAdmin):
+    list_display = ('id', 'data')
+
+@admin.register(ItemVenda)
+class ItemVendaAdm(admin.ModelAdmin):
+    list_display = ('id', 'qtde')
--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Perfil(models.Model):
+    telefone = models.CharField(max_length=25, null=True, blank=True)
+    rua = models.CharField(max_length=100, null=True, blank=True)
+    numero = models.PositiveIntegerField(null=True, blank=True)
+    bairro = models.CharField(max_length=50, null=True, blank=True)
+    cidade = models.CharField(max_length=50, null=True, blank=True)
+    complemento = models.CharField(max_length=100, null=True, blank=True)
+    cep = models.CharField(max_length=50, null=True, blank=True)
+    cliente = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Perfil de {self.cliente.username}'
+
+class Venda(models.Model):
+    data = models.DateField(auto_now_add=True)
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.data
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100, null=True, blank=True)
@@ -7,5 +28,17 @@ class Produto(models.Model):
     descricao = models.TextField(null=True, blank=True)
     data = models.DateField(null=True, blank=True)
     imagem = models.ImageField(upload_to='produtos/', null=True, blank=True)
+
     def __str__(self):
         return self.nome
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    qtde = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.produto.nome}'
+
+
+
